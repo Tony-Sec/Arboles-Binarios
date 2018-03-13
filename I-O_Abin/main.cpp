5c2f57238893ff6dc19739648680ed8429f000b2
Zayk01 <37192501+Zayk01@users.noreply.github.com>
2018-03-13 06:56:04 +0100

    Update main.cpp

--- I-O_Abin/main.cpp
+++ I-O_Abin/main.cpp
@@ -9,14 +9,15 @@ const T_elem fin = '#';
 int main() {
 
     Abin<T_elem> A (20), B(20);
-/*
+/********************************************************Guardar Arbol binario en abin.dat***************************************/
     cout << "Introducir A_bin --> abin.dat" << endl;
     rellenarAbin(A,fin);            //std::cin
     ofstream file_out("abin.dat");
     imprimirAbin(file_out,A,fin);
     file_out.close();
     cout << "\n*** A_bin guardado en abin.dat ***"<<endl;
-*/
+    
+/********************************************************Leer Arbol binario en abin.dat******************************************/
     cout <<"abin.dat --> Extraer A_bin"<<endl;
     ifstream file_input("abin.dat");
     rellenarAbin(file_input,B);     //std::cout
