f81f2e348ba7d2122b0a903f075f87cd5fd5b385
Zayk01 <37192501+Zayk01@users.noreply.github.com>
2018-03-18 13:19:00 +0100

    Add exceptions

--- A_bin_dinamico.hpp
+++ A_bin_dinamico.hpp
@@ -3,6 +3,8 @@
 
 #include <cassert>
 
+using namespace std;
+
 template<typename T>
 class Abin {
     struct celda;
@@ -11,41 +13,31 @@ public:
     typedef celda *nodo;
     static const nodo NODO_NULO;
 
-    Abin();
+    class Error{
+    public:
+        Error(const char* c):porque(c){}
+        const char* why() const{return porque;};
+    private:
+        const char* porque;
+    };
 
-// constructor
+
+    Abin();// constructor
     void insertarRaizB(const T &e);
-
     void insertarHijoIzqdoB(nodo n, const T &e);
-
     void insertarHijoDrchoB(nodo n, const T &e);
-
     void eliminarHijoIzqdoB(nodo n);
-
     void eliminarHijoDrchoB(nodo n);
-
     void eliminarRaizB();
-
-    ~Abin();
-
-// destructor
+    ~Abin();// destructor
     bool arbolVacioB() const;
-
     const T &elemento(nodo n) const; // acceso a elto, lectura
-    T &elemento(nodo n);
-
-// acceso a elto, lectura/escritura
+    T &elemento(nodo n);// acceso a elto, lectura/escritura
     nodo raizB() const;
-
     nodo padreB(nodo n) const;
-
     nodo hijoIzqdoB(nodo n) const;
-
     nodo hijoDrchoB(nodo n) const;
-
-    Abin(const Abin<T> &a);
-
-// ctor. de copia
+    Abin(const Abin<T> &a);// ctor. de copia
     Abin<T> &operator=(const Abin<T> &a); //asignación de árboles
 private:
     struct celda {
@@ -73,61 +65,50 @@ inline Abin<T>::Abin() : r(NODO_NULO) {}
 
 template<typename T>
 inline void Abin<T>::insertarRaizB(const T &e) {
-    assert(r == NODO_NULO);
-// árbol vacío
+    if(r == NODO_NULO) throw Error("Arbol vacio");
     r = new celda(e);
 }
 
 template<typename T>
-inline void Abin<T>::insertarHijoIzqdoB(Abin<T>::nodo n,
-                                        const T &e) {
-    assert(n != NODO_NULO);
-    assert(n->hizq == NODO_NULO);
-// no existe hijo
+inline void Abin<T>::insertarHijoIzqdoB(Abin<T>::nodo n,const T &e) {
+    if(n != NODO_NULO) throw Error("Imposible insertar Hijo Izq :el nodo no es valido,debe ser nulo");
+    if(n->hizq == NODO_NULO)throw Error("Imposible insertar Hijo Izq :hijo izq no existe");
     n->hizq = new celda(e, n);
 }
 
 template<typename T>
-inline
-void Abin<T>::insertarHijoDrchoB(Abin<T>::nodo n, const T &e) {
-    assert(n != NODO_NULO);
-    assert(n->hder == NODO_NULO); // no existe hijo
+inline void Abin<T>::insertarHijoDrchoB(Abin<T>::nodo n, const T &e) {
+    if(n != NODO_NULO) throw Error("Imposible insertar Hijo Der :el nodo no es valido,debe ser nulo");
+    if(n->hder == NODO_NULO)throw Error("Imposible insertar Hijo Der:hijo der no existe");
     n->hder = new celda(e, n);
 }
 
 template<typename T>
 inline void Abin<T>::eliminarHijoIzqdoB(Abin<T>::nodo n) {
-    assert(n != NODO_NULO);
-    assert(n->hizq != NODO_NULO);
-// existe hijo izqdo.
-    assert(n->hizq->hizq == NODO_NULO &&
-           // hijo izqdo.
-           n->hizq->hder == NODO_NULO);
-// es hoja
+    if(n != NODO_NULO) throw Error("Imposible eliminar Hijo Izq :el nodo no es valido,debe ser nulo");
+    if(n->hizq != NODO_NULO)throw Error("Imposible eliminar Hijo Izq:el nodo tiene hijo izq");
+
+    if(n->hizq->hizq == NODO_NULO &&n->hizq->hder == NODO_NULO)
+        throw Error("Imposible eliminar Hijo Izq:El  hijo izq del nodo es hoja");
     delete (n->hizq);
     n->hizq = NODO_NULO;
 }
 
 template<typename T>
 inline void Abin<T>::eliminarHijoDrchoB(Abin<T>::nodo n) {
-    assert(n != NODO_NULO);
-    assert(n->hder != NODO_NULO);
-// existe hijo drcho.
-    assert(n->hder->hizq == NODO_NULO &&
-           // hijo drcho.
-           n->hder->hder == NODO_NULO);
-// es hoja
+    if(n != NODO_NULO) throw Error("Imposible eliminar Hijo Der :el nodo no es valido,debe ser nulo");
+    if(n->hder != NODO_NULO)throw Error("Imposible eliminar Hijo Der:el nodo tiene hijo der");
+    if(n->hder->hizq == NODO_NULO && n->hder->hder == NODO_NULO)
+        throw Error("Imposible eliminar Hijo Der:El  hijo der del nodo es hoja");
     delete (n->hder);
     n->hder = NODO_NULO;
 }
 
 template<typename T>
 inline void Abin<T>::eliminarRaizB() {
-    assert(r != NODO_NULO);
-// árbol no vacío
-    assert(r->hizq == NODO_NULO &&
-           r->hder == NODO_NULO);
-// la raíz es hoja
+    if(r != NODO_NULO)throw Error("Imposible eliminar Raiz :Arbol no vacío");
+    if(r->hizq == NODO_NULO && r->hder == NODO_NULO)
+        throw Error("Imposible eliminar Raiz:La raiz es hoja");
     delete (r);
     r = NODO_NULO;
 }
@@ -185,15 +166,14 @@ inline Abin<T>::Abin(const Abin<T> &a) {
 
 template<typename T>
 Abin<T> &Abin<T>::operator=(const Abin<T> &a) {
-    if (this != &a)
-// evitar autoasignación
-    {
-        this->~Abin();
-// vaciar el árbol
+    if (this != &a){// evitar autoasignación
+        this->~Abin();// vaciar el árbol
         r = copiar(a.r);
     }
     return *this;
-}// Métodos privados
+}
+
+// Métodos privados
 // Destruye un nodo y todos sus descendientes
 template<typename T>
 void Abin<T>::destruirNodos(Abin<T>::nodo &n) {
@@ -203,13 +183,14 @@ void Abin<T>::destruirNodos(Abin<T>::nodo &n) {
         delete n;
         n = NODO_NULO;
     }
-}// Devuelve una copia de un nodo y todos sus descendientes
+}
+
+// Devuelve una copia de un nodo y todos sus descendientes
 template<typename T>
 typename Abin<T>::nodo Abin<T>::copiar(Abin<T>::nodo n) {
     nodo m = NODO_NULO;
     if (n != NODO_NULO) {
-        m = new celda(n->elto);
-// copiar n
+        m = new celda(n->elto);// copiar n
         m->hizq = copiar(n->hizq); // copiar subárbol izqdo.
         if (m->hizq != NODO_NULO)
             m->hizq->padre = m;
